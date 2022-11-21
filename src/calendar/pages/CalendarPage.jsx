13afb2829615f4828bd0d401e2615d009c947a8f
NicolasValdes27 <nicolasignacio.vq@gmail.com>
2022-11-21 20:13:18 -0300

    Calendar initialization

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,48 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns'
+import { localizer, getMessagesES } from '../../helpers';
 import { Navbar } from "../"
 
+const events = [
+    {
+        title: 'Cumpleaños',
+        notes: 'Pastel comprar',
+        start: new Date(),
+        end: addHours(new Date(), 2),
+        bgColor: '#fafafa',
+    }
+]
+
 export const CalendarPage = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#347cf7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <>
             <Navbar />
+            <Calendar
+                culture='es'
+                localizer={ localizer }
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={ getMessagesES() }
+                eventPropGetter={ eventStyleGetter }
+            />
         </>
     )
 }
